15e950717079c28ee657d7657e35192124a166b3
Guy Whorley <guy.whorley@cherwell.com>
2018-12-15 21:03:50 -0800

    saving

--- LearningWPF/MainWindow.xaml.cs
+++ LearningWPF/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using static LearningWPF.DataBindingOneWay.Employee;
 
 namespace LearningWPF
 {
@@ -23,6 +11,54 @@ namespace LearningWPF
 		public MainWindow()
 		{
 			InitializeComponent();
+			DataContext = GetEmployee();
+		}
+
+		private void OnClick(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Button is clicked");
+		}
+
+
+		private void Bold_Checked(object sender, RoutedEventArgs e)
+		{
+			textBox1.FontWeight = FontWeights.Bold;
+		}
+
+		private void Bold_Unchecked(object sender, RoutedEventArgs e)
+		{
+			textBox1.FontWeight = FontWeights.Normal;
+		}
+
+		private void Italic_Checked(object sender, RoutedEventArgs e)
+		{
+			textBox1.FontStyle = FontStyles.Italic;
+		}
+
+		private void Italic_Unchecked(object sender, RoutedEventArgs e)
+		{
+			textBox1.FontStyle = FontStyles.Normal;
+		}
+
+		private void IncreaseFont_Click(object sender, RoutedEventArgs e)
+		{
+			if (textBox1.FontSize < 18)
+			{
+				textBox1.FontSize += 2;
+			}
+		}
+
+		private void DecreaseFont_Click(object sender, RoutedEventArgs e)
+		{
+			if (textBox1.FontSize > 10)
+			{
+				textBox1.FontSize -= 2;
+			}
+		}
+
+		private void ContextMenu_Close(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("You just closed the context menu");
 		}
 	}
 }
